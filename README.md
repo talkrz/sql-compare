7c3268fed292d707e7b1b8fc25c3f38dc2b72e4f
ktalajko <krzysztof.talajko@schibsted.pl>
2015-10-20 21:57:55 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # sql-compare
-Command line tool for comparing databases schema. Built un top of Doctrine DBAL
+Command line tool for comparing databases schema, built upon Doctrine DBAL
+
+## Usage
+
+TODO: Build phar archive of the applicaiton, and describe how to download it.
+
+To compare two MySQL databases type:
+```bash
+bin/sql-compare mysql://user:secret@server/db1 mysql://user:secret@server/db2
+```
+
+You can use any database URL allowed by Doctrine DBAL. Look ad the docs for
+more details:
+http://docs.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html#connecting-using-a-url
---
+++ src/SqlCompare/SchemaComparator.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace SqlCompare;
+
+use Doctrine\DBAL\Schema\Schema,
+    Doctrine\DBAL\Configuration,
+    Doctrine\DBAL\Connection,
+    Doctrine\DBAL\Schema\Comparator,
+    Doctrine\DBAL\DriverManager;
+
+class SchemaComparator
+{
+    /**
+     * Configuration for Doctrine DBAL
+     * @var \Doctrine\DBAL\Configuration
+     */
+    private $config;
+
+    /**
+     * First database url
+     * @var string
+     */
+    private $leftDbUrl;
+
+    /**
+     * Second database url
+     * @var string
+     */
+    private $rightDbUrl;
+
+    /**
+     * Comparator is built by providing two databases URLs
+     *
+     * @param string $leftDbUrl  value allowed by Doctrine DBAL
+     * @param string $rightDbUrl value allowed by Doctrine DBAL
+     */
+    public function __construct($leftDbUrl, $rightDbUrl)
+    {
+        $this->config = new Configuration();
+        $this->leftDbUrl = $leftDbUrl;
+        $this->rightDbUrl = $rightDbUrl;
+    }
+
+    /**
+     * Get SQLs to convert from left to right database
+     * Convention is tool returns queries to convert left db to right one,
+     * so queries are obtained based on left database platform
+     *
+     * @return array
+     */
+    public function diff()
+    {
+        $leftConnection = $this->createConnection($this->leftDbUrl);
+        $rightConnection = $this->createConnection($this->rightDbUrl);
+        $leftSchema = $leftConnection->getSchemaManager()
+            ->createSchema();
+        $rightSchema = $rightConnection->getSchemaManager()
+            ->createSchema();
+
+        $comparator = new Comparator();
+        $schemaDiff = $comparator->compare($leftSchema, $rightSchema);
+
+        $leftPlatform = $leftConnection->getDriver()
+            ->getDatabasePlatform();
+
+        return $schemaDiff->toSql($leftPlatform);
+    }
+
+    /**
+     * Create Doctrine DBAL schema object
+     * @param  string $dbUrl db url allowed for Doctrine DBAL
+     * @return Connection
+     */
+    private function createConnection($dbUrl)
+    {
+        $connectionParams = array(
+            'url' => $dbUrl,
+        );
+        return DriverManager::getConnection($connectionParams, $this->config);
+    }
+}
